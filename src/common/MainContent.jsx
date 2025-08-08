731607e0d8e537b1b0c6554ae3e5d36eca65b6af
adarshsharma3 <adarshsharma300704@gmail.com>
2025-08-08 09:52:58 +0530

    changes

--- src/common/MainContent.jsx
+++ src/common/MainContent.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Header } from './Header';
 import { Dashboard } from './Dashboard';
 
 export const MainContent = () => {
   return (
     <div className="flex-1 flex flex-col bg-gray-50">
-      <Header 
-        
-      />
+    
       <Dashboard />
     </div>
   );
